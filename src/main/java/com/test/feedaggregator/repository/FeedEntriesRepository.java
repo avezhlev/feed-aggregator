3b6ea268b2c89644d79bee073eeed8482560e8e7
Alexandr Vezhlev <avezhlev@gmail.com>
2019-04-18 04:00:30 +0500

    Made retrieval of entries by title part to be transactional

--- src/main/java/com/test/feedaggregator/repository/FeedEntriesRepository.java
+++ src/main/java/com/test/feedaggregator/repository/FeedEntriesRepository.java
@@ -4,12 +4,14 @@ import com.test.feedaggregator.entity.FeedEntry;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface FeedEntriesRepository extends JpaRepository<FeedEntry, Long> {
 
+    @Transactional(readOnly = true)
     List<FeedEntry> findByTitleIgnoreCaseContaining(String title, Sort sort);
 
 }
